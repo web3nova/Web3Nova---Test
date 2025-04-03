a1af935e4f5469d33f633dcee670d12d2ae1254b
Adebanjo Abraham.I <navigatorabraham17@gmail.com>
2025-04-03 17:25:22 +0100

    test

--- AdebanjoAbraham/smartcontract/votingsystem.sol
+++ AdebanjoAbraham/smartcontract/votingsystem.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
+/*A decentralized Voting System allows users to:**
 
+**Create an election with multiple candidates.
+Vote for a candidate in a specific election.
+Track votes to ensure users can only vote once per election.
+Retrieve election candidates and voters who participated.
+The system must support multiple elections and store election details securely on the blockchain.*/
 
 contract votingsystem {
 
@@ -12,6 +18,7 @@ struct voter {
 }
 mapping (string => voter) public voters;
 
+
 function createElection(string memory _electionName, string[] memory _candidateNames) public{
 voters[_electionName] = voter({
     electionname: _electionName,
@@ -19,9 +26,15 @@ voters[_electionName] = voter({
 });
 }
 
- function vote(string memory _electionName, uint _candidateIndex) public{
-    string memory newvote = voters[_electionName].candidatesname[_candidateIndex];
-    
+ function vote(string memory _electionName, string memory _candidatename) public{
+    string memory newvote = voters[_electionName].candidatesname[_candidatename];
+ } 
 
- }  
+ function  getCandidates(uint _electionId) → Candidate[] public view returns (Candidate[] memory) {
+    return elections[_electionId].candidates;
+}
+
+function getVoters(uint _electionId) → address[] public view returns (address[] memory) {
+    return elections[_electionName].voters;
+}
 }
